8813a75431ddd77dde94ad54453ed3242b67f636
Sibogoje <sibandzesiboniso0@gmail.com>
2022-10-29 08:51:18 +0530

    ff

--- header.php
+++ header.php
@@ -130,7 +130,7 @@
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="logout.php">
+              <a class="dropdown-item d-flex align-items-center" href=" header('Location: https://super.shippii.online/logout.php');">
                 <i class="bi bi-box-arrow-right"></i>
                 <span>Sign Out</span>
               </a>
