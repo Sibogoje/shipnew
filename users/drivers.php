dde93f4c08ee33f9e658771cf6742fa0d96195e4
Sibogoje <sibandzesiboniso0@gmail.com>
2022-10-30 12:29:52 +0530

    s

--- users/drivers.php
+++ users/drivers.php
@@ -73,9 +73,9 @@ if (isset($_POST['savenew'])){
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.12.1/b-2.2.3/b-html5-2.2.3/b-print-2.2.3/date-1.1.2/fh-3.2.4/r-2.3.0/rg-1.2.0/sc-2.0.7/sb-1.3.4/sp-2.0.2/sl-1.4.0/datatables.min.css"/>
   <link href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css" rel="stylesheet">
   <link href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css" rel="stylesheet">
-  
-  <link href='select2/dist/css/select2.min.css' rel='stylesheet' type='text/css'>
   <script src='select2/dist/js/select2.min.js' type='text/javascript'></script>
+  <link href='select2/dist/css/select2.min.css' rel='stylesheet' type='text/css'>
+
   <!-- Template Main CSS File -->
   <link href="../assets/css/style.css" rel="stylesheet">
 
